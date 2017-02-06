8eef83b43502dde816a847cdc3bc635b61e18494
dongjun <786549629@qq.com>
2017-02-06 11:46:59 +0800

    null

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('a').each(function (i, e) {
-        var oldHref = $(e).attr('href') || '';
-        var href = oldHref.match(/(\/)(.*)/);
-        href = RegExp.$2;
-        // console.log('href --------', href)
-        $(e).attr('href', href)
-    })
-})
